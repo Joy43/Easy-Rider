e532ccd7a3b754cc01c172b693c2ce1a826b4421
Joy43 <ssjoy43@gmail.com>
2024-08-21 13:15:52 +0600

    signup page complete

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,13 @@
 import React from 'react'
 
 import { Redirect } from 'expo-router'
+import { useAuth } from '@clerk/clerk-expo'
 
 export default function Home() {
+  const {isSignedIn}=useAuth()
+  if(isSignedIn){
+    return <Redirect href="/(root)/(tabs)/home"/>
+  }
   return (
    <Redirect href="/(auth)/welcome"></Redirect>
   )
